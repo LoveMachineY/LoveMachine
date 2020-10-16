c7be2e5057ede598179d55ed85cae8243fc6b634
LoveMachineY <54717893+LoveMachineY@users.noreply.github.com>
2020-10-17 04:00:23 +0800

    Add files via upload

--- BattleRoyale.java
+++ BattleRoyale.java
@@ -1,7 +1,19 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class BattleRoyale {
     public static void main(String[] args){
+
+        SuperHero heroA = new SuperHero(new String[16]), heroB = new SuperHero(new String[16]);
+        int damageA = heroA.attack(heroB);
+        int damageB = heroB.attack(heroA);
+
+        ArrayList<SuperHero> heroes;
+        for(int i=0; i<heroes.size();i++){
+            for(int j=0; j<heroes.size();i++){
+
+            }
+        }
 
         PrintWriter writer;
         try{
